71907f442e45248e161c25060e5f30625b3e5eda
Nikita-Komintsev <nikita.comin@gmail.com>
2022-03-31 22:38:22 +0300

    commit

--- Lesson6/internalfilestorage/src/main/java/com/mirea/komintsev/internalfilestorage/MainActivity.java
+++ Lesson6/internalfilestorage/src/main/java/com/mirea/komintsev/internalfilestorage/MainActivity.java
@@ -2,13 +2,75 @@ package com.mirea.komintsev.internalfilestorage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String fileName = "mirea.txt";
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+
+        String string = "Hello mirea!";
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                tv.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(getTextFromFile());
+                    }
+                });
+            }
+        }).start();
+    }
+    // открытие файла
+    private String getTextFromFile() {
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(fileName);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            Log.d(LOG_TAG, text);
+            return text;
+        }catch (IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return null;
     }
 }
