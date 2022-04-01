67b51365c4130fce9cc7f9dd75acb02492f5d3e3
Nikita-Komintsev <nikita.comin@gmail.com>
2022-04-01 12:31:52 +0300

    commit

---
+++ Lesson6/room/src/main/java/com/mirea/komintsev/room/Employee.java
@@ -0,0 +1,12 @@
+package com.mirea.komintsev.room;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Employee {
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+    public String name;
+    public int salary;
+}
